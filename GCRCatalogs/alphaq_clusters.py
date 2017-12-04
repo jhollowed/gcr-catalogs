37307d62a5aefbd52fba7f2bd3aa60e2365dd171
Joe Hollowed <jphollowed@gmail.com>
2017-12-04 13:10:18 -0600

    new yaml file for cluster catalog
    new clusters yaml files
    
    separating cluster reader from standard protodc2 reader

---
+++ GCRCatalogs/alphaq_clusters.py
@@ -0,0 +1,227 @@
+"""
+Alpha Q galaxy catalog class.
+"""
+from __future__ import division
+import os
+import numpy as np
+import h5py
+from astropy.cosmology import FlatLambdaCDM
+from GCR import BaseGenericCatalog
+import pdb
+
+__all__ = ['AlphaQGalaxyCatalog', 'AlphaQClusterCatalog']
+
+
+class AlphaQGalaxyCatalog(BaseGenericCatalog):
+    """
+    Alpha Q galaxy catalog class. Uses generic quantity and filter mechanisms
+    defined by BaseGenericCatalog class.
+    """
+
+    def _subclass_init(self, filename, **kwargs):
+
+        assert os.path.isfile(filename), 'Catalog file {} does not exist'.format(filename)
+        self._file = filename
+        self.lightcone = kwargs.get('lightcone')
+
+        with h5py.File(self._file, 'r') as fh:
+            self.cosmology = FlatLambdaCDM(
+                H0=fh['metaData/simulationParameters/H_0'].value,
+                Om0=fh['metaData/simulationParameters/Omega_matter'].value,
+                Ob0=fh['metaData/simulationParameters/Omega_b'].value,
+            )
+            try:
+                catalog_version = '{}.{}'.format(
+                    fh['metaData/versionMajor'].value,
+                    fh['metaData/versionMinor'].value,
+                )
+            except KeyError:
+                #If no version is specified, it's version 2.0
+                catalog_version = '2.0'
+
+        config_version = kwargs.get('version', '')
+        if config_version != catalog_version:
+            raise ValueError('Catalog file version {} does not match config version {}'.format(catalog_version, config_version))
+
+        self._quantity_modifiers = {
+            'galaxy_id' :         'galaxyID',
+            'ra':                 'ra',
+            'dec':                'dec',
+            'ra_true':            'ra_true',
+            'dec_true':           'dec_true',
+            'redshift':           'redshift',
+            'redshift_true':      'redshiftHubble',
+            'disk_sersic_index':  'diskSersicIndex',
+            'bulge_sersic_index': 'spheroidSersicIndex',
+            'shear_1':            'shear1',
+            'shear_2':            'shear2',
+            'convergence':        'convergence',
+            'magnification':      'magnification',
+            'halo_id':            'hostIndex',
+            'halo_mass':          'hostHaloMass',
+            'is_central':         (lambda x : x.astype(np.bool), 'isCentral'),
+            'stellar_mass':       'totalMassStellar',
+            'size_disk_true':     'morphology/diskHalfLightRadius',
+            'size_bulge_true':    'morphology/spheroidHalfLightRadius',
+            'position_x':         'x',
+            'position_y':         'y',
+            'position_z':         'z',
+            'velocity_x':         'vx',
+            'velocity_y':         'vy',
+            'velocity_z':         'vz',
+        }
+
+        if catalog_version == '2.0': # to be backward compatible
+            self._quantity_modifiers.update({
+                'ra':       (lambda x: x/3600, 'ra'),
+                'ra_true':  (lambda x: x/3600, 'ra_true'),
+                'dec':      (lambda x: x/3600, 'dec'),
+                'dec_true': (lambda x: x/3600, 'dec_true'),
+            })
+
+        for band in 'ugriz':
+            self._quantity_modifiers['mag_{}_lsst'.format(band)] = 'LSST_filters/magnitude:LSST_{}:observed'.format(band)
+            self._quantity_modifiers['mag_{}_sdss'.format(band)] = 'SDSS_filters/magnitude:SDSS_{}:observed'.format(band)
+            self._quantity_modifiers['Mag_true_{}_lsst_z0'.format(band)] = 'LSST_filters/magnitude:LSST_{}:rest'.format(band)
+            self._quantity_modifiers['Mag_true_{}_sdss_z0'.format(band)] = 'SDSS_filters/magnitude:SDSS_{}:rest'.format(band)
+
+        self._quantity_modifiers['mag_Y_lsst'] = 'LSST_filters/magnitude:LSST_y:observed'
+        self._quantity_modifiers['Mag_true_Y_lsst_z0'] = 'LSST_filters/magnitude:LSST_y:rest'
+
+        with h5py.File(self._file, 'r') as fh:
+            self.cosmology = FlatLambdaCDM(
+                H0=fh['metaData/simulationParameters/H_0'].value,
+                Om0=fh['metaData/simulationParameters/Omega_matter'].value,
+                Ob0=fh['metaData/simulationParameters/Omega_b'].value
+            )
+
+
+    def _generate_native_quantity_list(self):
+        with h5py.File(self._file, 'r') as fh:
+            hgroup = fh['galaxyProperties']
+            hobjects = []
+            #get all the names of objects in this tree
+            hgroup.visit(hobjects.append)
+            #filter out the group objects and keep the dataste objects
+            hdatasets = [hobject for hobject in hobjects if type(hgroup[hobject]) == h5py.Dataset]
+            native_quantities = set(hdatasets)
+        return native_quantities
+
+
+    def _iter_native_dataset(self, native_filters=None):
+        assert not native_filters, '*native_filters* is not supported'
+        with h5py.File(self._file, 'r') as fh:
+            def native_quantity_getter(native_quantity):
+                return fh['galaxyProperties/{}'.format(native_quantity)].value
+            yield native_quantity_getter
+
+
+    def _get_native_quantity_info_dict(self, quantity, default=None):
+        with h5py.File(self._file,'r') as fh:
+            if 'galaxyProperties/'+quantity not in fh:
+                return default
+            else:
+                info_dict = dict()
+                for key in fh['galaxyProperties/'+quantity].attrs:
+                    info_dict[key] = fh['galaxyProperties/'+quantity].attrs[key]
+                return info_dict
+
+
+    def _get_quantity_info_dict(self, quantity, default=None):
+        return default
+        #TODO needs some fixing
+        # print "in get quantity"
+        # native_name = None
+        # if quantity in self._quantity_modifiers:
+        #     print "in quant modifers"
+        #     q_mod = self._quantity_modifiers[quantity]
+        #     if isinstance(q_mod,(tuple,list)):
+        #         print "it's a list object, len:",len(length)
+
+        #         if(len(length) > 2):
+        #             return default #This value is composed of a function on
+        #             #native quantities. So we have no idea what the units are
+        #         else:
+        #             #Note: This is just a renamed column.
+        #             return self._get_native_quantity_info_dict(q_mod[1],default)
+        #     else:
+        #         print "it's a string: ",q_mod
+        #         return self._get_native_quantity_info_dict(q_mod,default)
+        # elif quantity in self._native_quantities:
+        #     print "in get native quant"
+        #     return self._get_native_quantity_info_dict(quantity,default)
+
+
+
+
+#=====================================================================================================
+
+class AlphaQClusterCatalog(AlphaQGalaxyCatalog):
+    """
+    The galaxy cluster catalog. Inherits AlphaQGalaxyCatalog, overloading select methods.
+
+    The AlphaQ cluster catalog is structured in the following way: under the root hdf group, there
+    is a group per each halo with SO mass above 1e14 M_sun/h. Each of these groups contains the same
+    datasets as the original AlphaQ galaxy catalog, but with only as many rows as member galaxies for
+    the halo in question. Each group has attributes which contain halo-wide quantities, such as mass,
+    position, etc.
+
+    This class offers filtering on any halo quantity (group attribute), as seen in all three of the
+    methods of this class (all the group attributes are iterated over in contexts concerning the
+    pre-filtering). The valid filtering quantities are:
+    {'host_halo_mass', 'sod_halo_cdelta', 'sod_halo_cdelta_error', 'sod_halo_c_acc_mass',
+     'fof_halo_tag', 'halo_index', 'halo_step', 'halo_ra', 'halo_dec', 'halo_z',
+     'halo_z_err', 'sod_halo_radius', 'sod_halo_mass', 'sod_halo_ke', 'sod_halo_vel_disp'}
+    """
+
+
+    def _subclass_init(self, filename, **kwargs):
+        super(AlphaQClusterCatalog, self)._subclass_init(filename, **kwargs)
+        pdb.set_trace()
+        with h5py.File(self._file, 'r') as fh:
+            self._native_filter_quantities = set(fh[next(fh.keys())].attrs)
+
+
+    def _iter_native_dataset(self, native_filters=None):
+        with h5py.File(self._file, 'r') as fh:
+            for key in fh:
+                halo = fh[key]
+
+                if native_filters and not all(f[0](*(halo.attrs[k] for k in f[1:])) for f in native_filters):
+                    continue
+
+                def native_quantity_getter(native_quantity):
+                    raise NotImplementedError
+
+                yield native_quantity_getter
+    
+    def _generate_native_quantity_list(self):
+        with h5py.File(self._file, 'r') as fh:
+            # quantities are the same for all halos - we use the first here
+            hgroup = fh['halo_1']
+            hobjects = list(hgroup.attrs.keys())
+            #get all the names of objects in this tree
+            hgroup.visit(hobjects.append)
+            #filter out the group objects and keep the dataset objects
+            hdatasets = [hobject for hobject in hobjects if type(hgroup[hobject]) != h5py.Group]
+            native_quantities = set(hdatasets)
+        return native_quantities
+
+
+    def _iter_native_dataset(self, native_filters=None):
+        assert not native_filters, '*native_filters* is not supported'
+        with h5py.File(self._file, 'r') as fh:
+            def native_quantity_getter(native_quantity):
+                return fh['galaxyProperties/{}'.format(native_quantity)].value
+            yield native_quantity_getter
+
+
+    def _get_native_quantity_info_dict(self, quantity, default=None):
+        with h5py.File(self._file,'r') as fh:
+            if 'galaxyProperties/'+quantity not in fh:
+                return default
+            else:
+                info_dict = dict()
+                for key in fh['galaxyProperties/'+quantity].attrs:
+                    info_dict[key] = fh['galaxyProperties/'+quantity].attrs[key]
+                return info_dict
